703d52425d997ed388228129efbba9398e95138b
FedericoTossani <fede.tossani@gmail.com>
2023-02-02 17:27:20 +0100

    Update R_code_wetlands.r

--- R_code_wetlands.r
+++ R_code_wetlands.r
@@ -48,17 +48,15 @@ list.of.packages <- c("tidyverse", "raster", "RStoolbox", "rasterdiv", "rasterVi
        ## Data import ##
 # =========================== #
 
-## I used this piece of code to create stack of bands I need for the further analysis
+# I used this piece of code to create stack of bands I need for the further analysis
 
-# list_img <- list.files(pattern = "191029_20050502")
+# list_img <- list.files(pattern = "193032_20200407")
 # img_allbands <- lapply(list_img, raster)
 # img_stack <- stack(img_allbands)
-# writeRaster(img_stack, filename="delta_p191r29_20050502.grd", format="raster")
+# writeRaster(img_stack, filename="oristano_p193r32_20200407.grd", format="raster")
 
 ## Import the multiband images with brick and crop it to the intrested area
 ## From now on I will use this images for the anlysis
-
-# Images of Sardinia
 
    ## Oristano
 
@@ -73,10 +71,10 @@ oristano_img <- lapply(oristano_list, brick)
 crop_file <- function(oristano_img) {
  
   # Create the extent object
-ext_oristano <- extent(447440, 463826.3, 4411123, 4428401)
+ ext_oristano <- extent(442938.9, 473086.2, 4389311, 4436271)
   
   # Crop the raster object with the extent
-  cropped_img <- crop(oristano_img, ext_cagliari)
+  cropped_img <- crop(oristano_img, ext_oristano)
   
   # Return the cropped raster object
   return(cropped_img)
@@ -91,7 +89,7 @@ remove_index <- c(TRUE, FALSE, TRUE, FALSE, TRUE, FALSE, TRUE, FALSE, TRUE, FALS
 # Remove the selected elements using the logical index
 oristano_cropped <- oristano_cropped[!remove_index]
 
-# plotRGB(oristano87_c, 4, 3, 2, stretch="lin")
+# plotRGB(oristano_cropped[[1]], 4, 3, 2, stretch="lin")
 
 
     ## Cagliari
@@ -162,6 +160,25 @@ delta_cropped <- delta_cropped[!remove_index]
        ## Spectral indices ##
 # =============================== #
 
+
+    ## Oristano indeces
+
+oristano88_indices <- RStoolbox::spectralIndices(oristano_cropped[[1]], blue = 1, green = 2, red = 3, nir = 4, swir2 = 5, swir3= 7, indices = c("NDVI", "NDWI", "NDWI2", "SLAVI"))
+oristano95_indices <- RStoolbox::spectralIndices(oristano_cropped[[2]], blue = 1, green = 2, red = 3, nir = 4, swir2 = 5, swir3= 7, indices = c("NDVI", "NDWI", "NDWI2", "SLAVI"))
+oristano02_indices <- RStoolbox::spectralIndices(oristano_cropped[[3]], blue = 1, green = 2, red = 3, nir = 4, swir2 = 5, swir3= 7, indices = c("NDVI", "NDWI", "NDWI2", "SLAVI"))
+oristano10_indices <- RStoolbox::spectralIndices(oristano_cropped[[4]], blue = 1, green = 2, red = 3, nir = 4, swir2 = 5, swir3= 7, indices = c("NDVI", "NDWI", "NDWI2", "SLAVI"))
+oristano20_indices <- RStoolbox::spectralIndices(oristano_cropped[[5]], blue = 2, green = 3, red = 4, nir = 5, swir2 = 6, swir3= 7, indices = c("NDVI", "NDWI", "NDWI2", "SLAVI"))
+
+
+    ## Cagliari indeces
+
+cagliari87_indices <- RStoolbox::spectralIndices(cagliari_cropped[[2]], blue = 1, green = 2, red = 3, nir = 4, swir2 = 5, swir3= 7, indices = c("NDVI", "NDWI", "NDWI2", "SLAVI"))
+cagliari92_indices <- RStoolbox::spectralIndices(cagliari_cropped[[3]], blue = 1, green = 2, red = 3, nir = 4, swir2 = 5, swir3= 7, indices = c("NDVI", "NDWI", "NDWI2", "SLAVI"))
+cagliari05_indices <- RStoolbox::spectralIndices(cagliari_cropped[[4]], blue = 1, green = 2, red = 3, nir = 4, swir2 = 5, swir3= 7, indices = c("NDVI", "NDWI", "NDWI2", "SLAVI"))
+cagliari11_indices <- RStoolbox::spectralIndices(cagliari_cropped[[5]], blue = 1, green = 2, red = 3, nir = 4, swir2 = 5, swir3= 7, indices = c("NDVI", "NDWI", "NDWI2", "SLAVI"))
+cagliari22_indices <- RStoolbox::spectralIndices(cagliari_cropped[[1]], blue = 2, green = 3, red = 4, nir = 5, swir2 = 6, swir3= 7, indices = c("NDVI", "NDWI", "NDWI2", "SLAVI"))
+
+
     ## Po delta indeces
 
 delta85_indices <- RStoolbox::spectralIndices(delta_cropped[[1]], blue = 1, green = 2, red = 3, nir = 4, swir2 = 5, swir3= 7, indices = c("NDVI", "NDWI", "NDWI2", "SLAVI"))
@@ -170,28 +187,121 @@ delta05_indices <- RStoolbox::spectralIndices(delta_cropped[[3]], blue = 1, gree
 delta11_indices <- RStoolbox::spectralIndices(delta_cropped[[4]], blue = 1, green = 2, red = 3, nir = 4, swir2 = 5, swir3= 7, indices = c("NDVI", "NDWI", "NDWI2", "SLAVI"))
 delta20_indices <- RStoolbox::spectralIndices(delta_cropped[[5]], blue = 2, green = 3, red = 4, nir = 5, swir2 = 6, swir3= 7, indices = c("NDVI", "NDWI", "NDWI2", "SLAVI"))
 
-    ## Cagliari indeces
-
-cagliari93_indices <- RStoolbox::spectralIndices(cagliari_cropped[[1]], blue = 1, green = 2, red = 3, nir = 4, swir2 = 5, swir3= 7, indices = c("NDVI", "NDWI", "NDWI2", "SLAVI"))
-cagliari02_indices <- RStoolbox::spectralIndices(cagliari_cropped[[2]], blue = 1, green = 2, red = 3, nir = 4, swir2 = 5, swir3= 7, indices = c("NDVI", "NDWI", "NDWI2", "SLAVI"))
-cagliari84_indices <- RStoolbox::spectralIndices(cagliari_cropped[[3]], blue = 1, green = 2, red = 3, nir = 4, swir2 = 5, swir3= 7, indices = c("NDVI", "NDWI", "NDWI2", "SLAVI"))
-cagliari11_indices <- RStoolbox::spectralIndices(cagliari_cropped[[4]], blue = 1, green = 2, red = 3, nir = 4, swir2 = 5, swir3= 7, indices = c("NDVI", "NDWI", "NDWI2", "SLAVI"))
-cagliari21_indices <- RStoolbox::spectralIndices(cagliari_cropped[[5]], blue = 2, green = 3, red = 4, nir = 5, swir2 = 6, swir3= 7, indices = c("NDVI", "NDWI", "NDWI2", "SLAVI"))
-
+    
 
 # ===================================================== #
        ## Land Use and Land Cover Classification ##
 # ===================================================== #
 
+
+    ## Oristano
+
+set.seed(999)
+oristano88_lcc <- unsuperClass(oristano_cropped[[1]], nSamples = 1000, nClasses = 5)
+set.seed(999)
+oristano95_lcc <- unsuperClass(oristano_cropped[[2]], nSamples = 1000, nClasses = 5)
+set.seed(999)
+oristano02_lcc <- unsuperClass(oristano_cropped[[3]], nSamples = 1000, nClasses = 5)
+set.seed(999)
+oristano10_lcc <- unsuperClass(oristano_cropped[[4]], nSamples = 1000, nClasses = 5)
+set.seed(999)
+oristano20_lcc <- unsuperClass(oristano_cropped[[5]], nSamples = 1000, nClasses = 5)
+
+oristano_lcc <- list(oristano88_lcc, oristano95_lcc, oristano02_lcc, oristano10_lcc, oristano20_lcc)
+
+par(mfrow = c(3, 2))
+plot(oristano_lcc[[1]]$map)
+plot(oristano_lcc[[2]]$map)
+plot(oristano_lcc[[3]]$map)
+plot(oristano_lcc[[4]]$map)
+plot(oristano_lcc[[5]]$map)
+
+
+    ## Cagliari
+
+set.seed(999)
+cagliari93_lcc <- unsuperClass(cagliari_cropped[[1]], nSamples = 1000, nClasses = 5)
+set.seed(999)
+cagliari02_lcc <- unsuperClass(cagliari_cropped[[2]], nSamples = 1000, nClasses = 5)
+set.seed(999)
+cagliari84_lcc <- unsuperClass(cagliari_cropped[[3]], nSamples = 1000, nClasses = 5)
+set.seed(999)
+cagliari11_lcc <- unsuperClass(cagliari_cropped[[4]], nSamples = 1000, nClasses = 5)
+set.seed(999)
+cagliari21_lcc <- unsuperClass(cagliari_cropped[[5]], nSamples = 1000, nClasses = 5)
+
+cagliari_lcc <- list(cagliari84_lcc, cagliari93_lcc, cagliari02_lcc, cagliari11_lcc, cagliari21_lcc)
+
+par(mfrow = c(3, 2))
+levelplot(cagliari_lcc[[1]]$map)
+levelplot(cagliari_lcc[[2]]$map)
+levelplot(cagliari_lcc[[3]]$map)
+levelplot(cagliari_lcc[[4]]$map)
+levelplot(cagliari_lcc[[5]]$map)
+
     ## Po delta
 
-delta85_lcc <- unsuperClass(delta_cropped[[1]], nSamples = 100, nClasses = 5)
-delta93_lcc <- unsuperClass(delta_cropped[[2]], nSamples = 100, nClasses = 5)
-delta05_lcc <- unsuperClass(delta_cropped[[3]], nSamples = 100, nClasses = 5)
-delta11_lcc <- unsuperClass(delta_cropped[[4]], nSamples = 100, nClasses = 5)
-delta20_lcc <- unsuperClass(delta_cropped[[5]], nSamples = 100, nClasses = 5)
+set.seed(999)
+delta85_lcc <- unsuperClass(delta_cropped[[1]], nSamples = 1000, nClasses = 5)
+set.seed(999)
+delta93_lcc <- unsuperClass(delta_cropped[[2]], nSamples = 1000, nClasses = 5)
+set.seed(999)
+delta05_lcc <- unsuperClass(delta_cropped[[3]], nSamples = 1000, nClasses = 5)
+set.seed(999)
+delta11_lcc <- unsuperClass(delta_cropped[[4]], nSamples = 1000, nClasses = 5)
+set.seed(999)
+delta20_lcc <- unsuperClass(delta_cropped[[5]], nSamples = 1000, nClasses = 5)
 
 delta_lcc <- list(delta85_lcc, delta93_lcc, delta05_lcc, delta11_lcc, delta20_lcc)
+
+par(mfrow = c(3, 2))
+plot(delta_lcc[[1]]$map)
+plot(delta_lcc[[2]]$map)
+plot(delta_lcc[[3]]$map)
+plot(delta_lcc[[4]]$map)
+plot(delta_lcc[[5]]$map)
+
+#set.seed(999)
+#par(mfrow = c(1, 2))
+#p1 <- plot(cagliari_lcc[[1]]$map)
+#p2 <- plot(cagliari_lcc[[5]]$map)
+
+#freq(cagliari_lcc[[1]]$map)
+#tot1 <- 122743 + 202457 + 315397 + 174396 + 222551 + 1060
+
+#prop1 <- freq(cagliari_lcc[[1]]$map)/tot1
+
+#tot2 <- 31649 + 175660 + 140770 + 309907 + 297884 + 82734
+#prop5 <- freq(cagliari_lcc[[5]]$map)/tot2
+
+
+# Load the data into a list
+#cagliari_lcc <- list(cagliari84_lcc, cagliari93_lcc, cagliari02_lcc, cagliari11_lcc, cagliari21_lcc)
+
+# Initialize a counter to keep track of the plot number
+#plot_num <- 1
+
+# Loop through the data in the list
+#for (i in 1:length(cagliari_lcc)) {
+  # Extract the current data
+#  data <- cagliari_lcc[[i]]
+  
+  # Create a plot using ggplot2
+#  p <- ggplot(data, aes(x = wt, y = mpg)) +
+#    geom_raster() +
+#    ggtitle(paste0("Plot ", plot_num))
+  
+  # Increment the plot number
+#  plot_num <- plot_num + 1
+  
+  # Show the plot
+#  print(p)
+#}
+
+
+
+
+
 
 # - # - # - # - # - # - # - # - # - # - # - # - # - # - # - #
 
@@ -208,64 +318,10 @@ delta_lcc <- list(delta85_lcc, delta93_lcc, delta05_lcc, delta11_lcc, delta20_lc
 #   delta_lcc[[i]] <- lcc
 # }
 
+# Errore in (function (classes, fdef, mtable)  : 
+#   non è possibile trovare un metodo ereditato per la funzione ‘nlayers’ per la firma ‘"NULL"’
+
 # - # - # - # - # - # - # - # - # - # - # - # - # - # - # - #
-
-    ## Cagliari
-
-set.seed(999)
-cagliari93_lcc <- unsuperClass(cagliari_cropped[[1]], nSamples = 100, nClasses = 5)
-cagliari02_lcc <- unsuperClass(cagliari_cropped[[2]], nSamples = 100, nClasses = 5)
-cagliari84_lcc <- unsuperClass(cagliari_cropped[[3]], nSamples = 100, nClasses = 5)
-cagliari11_lcc <- unsuperClass(cagliari_cropped[[4]], nSamples = 100, nClasses = 5)
-cagliari21_lcc <- unsuperClass(cagliari_cropped[[5]], nSamples = 100, nClasses = 5)
-
-cagliari_lcc <- list(cagliari84_lcc, cagliari93_lcc, cagliari02_lcc, cagliari11_lcc, cagliari21_lcc)
-
-
-
-
-set.seed(999)
-par(mfrow = c(1, 2))
-p1 <- plot(cagliari_lcc[[1]]$map)
-p2 <- plot(cagliari_lcc[[5]]$map)
-
-freq(cagliari_lcc[[1]]$map)
-tot1 <- 122743 + 202457 + 315397 + 174396 + 222551 + 1060
-
-prop1 <- freq(cagliari_lcc[[1]]$map)/tot1
-
-tot2 <- 31649 + 175660 + 140770 + 309907 + 297884 + 82734
-prop5 <- freq(cagliari_lcc[[5]]$map)/tot2
-
-
-
-
-
-
-# Load the data into a list
-cagliari_lcc <- list(cagliari84_lcc, cagliari93_lcc, cagliari02_lcc, cagliari11_lcc, cagliari21_lcc)
-
-# Initialize a counter to keep track of the plot number
-plot_num <- 1
-
-# Loop through the data in the list
-for (i in 1:length(cagliari_lcc)) {
-  # Extract the current data
-  data <- cagliari_lcc[[i]]
-  
-  # Create a plot using ggplot2
-  p <- ggplot(data, aes(x = wt, y = mpg)) +
-    geom_raster() +
-    ggtitle(paste0("Plot ", plot_num))
-  
-  # Increment the plot number
-  plot_num <- plot_num + 1
-  
-  # Show the plot
-  print(p)
-}
-
-
 
 
 
